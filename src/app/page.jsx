e2224997a5c89f2ffa6e34598b41e8c333af4874
Zeta <80638821+darcko-dev@users.noreply.github.com>
2023-10-20 04:23:20 +0000

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,24 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
-      <body>
-        <nav>
-          <a href="/">Inicio</a>
-        </nav>
-      </body>
+      <nav className="text-center p-px">
+        <div className="bg-white text-black m-4 rounded-full">
+          <Link className="m-4" href="/">
+            Inicio
+          </Link>
+          <Link className="m-4" href="/">
+            Sobre Mi
+          </Link>
+          <a className="m-4" href="/">
+            Contactame
+          </a>
+          <a className="m-4" href="/">
+            Proyectos
+          </a>
+        </div>
+      </nav>
     </main>
   );
 }
